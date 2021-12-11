fdd842fd939ed3864f5f42e26e76a91da7b63eb1
LeoGianluca <leogianluca.costa@gmail.com>
2021-12-11 11:28:02 -0300

    Desenvolvimento das regras de negócio

--- README.md
+++ README.md
@@ -38,12 +38,12 @@ Instância do servidor na porta 3030 (http://localhost:3030)
 - [x] Deve ser possível criar uma conta;
 - [x] Deve ser possível buscar o extrato bancário do cliente;
 - [x] Deve ser possível realizar um depósito;
-- [] Deve ser possível realizar um saque;
-- [] Deve ser possível buscar o extrato bancário do cliente por data;
-- [] Deve ser possível atualizar dados da conta do cliente;
-- [] Deve ser possível obter dados da conta do cliente;
-- [] Deve ser possível deletar uma conta;
-- [] Deve ser possível retornar o balanço.
+- [x] Deve ser possível realizar um saque;
+- [x] Deve ser possível buscar o extrato bancário do cliente por data;
+- [x] Deve ser possível atualizar dados da conta do cliente;
+- [X] Deve ser possível obter dados da conta do cliente;
+- [X] Deve ser possível deletar uma conta;
+- [X] Deve ser possível retornar o balanço.
 
 ---
 
@@ -51,7 +51,7 @@ Instância do servidor na porta 3030 (http://localhost:3030)
 
 - [x] Não deve ser possível cadastrar uma conta com CPF já exístente;
 - [x] Não deve ser possível buscar extrato em uma conta não exístente;
-- [] Não deve ser possível fazer depósito em uma conta não exístente;
-- [] Não deve ser possível fazer saque em uma conta não exístente;
-- [] Não deve ser possível fazer saque quando o saldo for insuficiente;
-- [] Não deve ser possível excluir uma conta não exístente.
+- [x] Não deve ser possível fazer depósito em uma conta não exístente;
+- [x] Não deve ser possível fazer saque em uma conta não exístente;
+- [x] Não deve ser possível fazer saque quando o saldo for insuficiente;
+- [X] Não deve ser possível excluir uma conta não exístente.
--- src/app.js
+++ src/app.js
@@ -11,23 +11,41 @@ app.use(express.json())
 const customers = []
 
 /**
- * Função Middleware de validação da existência de um CPF cadastrado. 
- * OBs: Middleware são funções que tem acesso ao objeto de
+ * Função Middleware de validação da existência de um  cadastrado.
+ * Obs: Middleware são funções que tem acesso ao objeto de
  * solicitação (req), o objeto de resposta (res).
- * 
+ *
  * @param {string} cpf - Número do Cadastro de Pessoas Física
  * @returns {[]}
  */
 verifyExistsAccount = (request, response, next) => {
   const { cpf } = request.headers
-  const customer = customers.find(customer => customer.cpf === cpf)
+  const customer = customers.find((customer) => customer.cpf === cpf)
 
-  if(!customer) {
-    return response.status(400).json({error: "Customer not found"})
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" })
   }
   request.customer = customer
 
   next()
+}
+
+/**
+ * Função Middleware responsável pela verificação de saldo existente.
+ *
+ * @param {string} statement - Extrato do cliente
+ * @returns {}
+ */
+getBalance = (statement) => {
+  const balance = statement.reduce((acc, operation) => {
+    if(operation.type === 'credit') {
+      return acc + operation.amount
+    } else {
+      return acc - operation.amount
+    }
+  }, 0)
+
+  return balance
 }
 
 /**
@@ -50,21 +68,129 @@ app.post("/account", (request, response) => {
   return response.status(201).send()
 })
 
+app.use(verifyExistsAccount)
 /**
- * Observação:
- * Caso todas as funções utilizem o mesmo escopo de verificação, utilizar: app.use(verifyExistsAccount). 
-*/
-
-/**
- * Função responsável pela listagem do extrado do cliente.
+ * Função responsável pela consulta do extrato do cliente.
  *
- * @param {string} cpf
+ * @param {string} cpf - Número do Cadastro de Pessoas Física
  * @returns {[]}
  */
-app.get('/statement', verifyExistsAccount, (request, response) => {   
+app.get("/statement", (request, response) => {
   const { customer } = request
 
   return response.json(customer.statement)
+})
+
+/**
+ * Função responsável pela criação de novo registro de depósito.
+ *
+ * @typedef Customers
+ * @property {string} description - Descrição do depósito
+ * @property {double} amount - Quantia do depósito
+ */
+app.post("/deposit", (request, response) => {
+  const { customer } = request
+  const { description, amount } = request.body
+
+  const statementOperation = { description, amount, created_at: new Date(), type: "credit" }
+  customer.statement.push(statementOperation)
+
+  return response.status(201).send()
+})
+
+/**
+ * Função responsável pela criação de novo registro de saque.
+ *
+ * @typedef Customers
+ * @property {string} amount - Quantia do saque
+ */
+app.post("/withdraw", (request, response) => {
+  const { customer } = request
+  const { amount } = request.body
+
+  const balance = getBalance(customer.statement)
+
+  if (balance < amount) {
+    return response.status(400).json({ error: "Insufficient funds!" })
+  }
+
+  const statementOperation = { amount, created_at: new Date(), type: "debit" }
+  customer.statement.push(statementOperation)
+
+  return response.status(201).send()
+})
+
+/**
+ * Função responsável pelo consulta do balanço do cliente.
+ *
+ * @typedef Customers
+ * @property {object} customer - Objeto contendo informações da conta do cliente
+ * @property {double} balance - Objeto contendo informação da quantia do cliente
+ */
+ app.get("/balance", (request, response) => {
+  const { customer } = request
+  const balance = getBalance(customer.statement)
+
+  return response.json(balance)
+})
+
+/**
+ * Função responsável pela consulta de extrato com filtro de data.
+ *
+ * @typedef Customers
+ * @property {date} date - Data para consulta
+ */
+app.get("/statement/date", (request, response) => {
+  const { customer } = request
+  const { date } = request.query
+
+  const statement = customer.statement.filter(
+    (statement) => statement.created_at.toISOString().includes(date)
+  )
+
+  return response.json(statement)
+})
+
+/**
+ * Função responsável pela atualização dos dados bancários.
+ *
+ * @typedef Customers
+ * @property {string} name - Nome do cliente
+ */
+app.put("/account", (request, response) => {
+  const { customer } = request
+  const { name } = request.body
+
+  customer.name = name
+
+  return response.status(201).send()
+})
+
+/**
+ * Função responsável pelo consulta dos dados da conta do cliente.
+ *
+ * @typedef Customers
+ * @property {object} customer - Objeto contendo informações da conta do cliente
+ */
+app.get("/account", (request, response) => {
+  const { customer } = request
+
+  return response.json(customer)
+})
+
+/**
+ * Função responsável pela remoção de uma conta de cliente.
+ *
+ * @typedef Customers
+ * @property {object} customer - Objeto contendo informações da conta do cliente
+ */
+app.delete("/account", (request, response) => {
+  const { customer } = request
+
+  const indexCustomer = customers.indexOf(customer)
+  customers.splice(indexCustomer, 1)
+
+  return response.status(200).json(customers)
 })
 
 module.exports = app
